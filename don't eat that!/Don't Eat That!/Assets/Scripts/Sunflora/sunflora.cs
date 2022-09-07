562440e8525fbc8111d0c93ef351df52227e90e5
GuhNA <89320187+GuhNA@users.noreply.github.com>
2022-09-07 11:24:08 -0300

    Zombie NavMesh

--- don't eat that!/Don't Eat That!/Assets/Scripts/Sunflora/sunflora.cs
+++ don't eat that!/Don't Eat That!/Assets/Scripts/Sunflora/sunflora.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sunflora : MonoBehaviour
+public class Sunflora : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float life;
+    [SerializeField] Animator anim;
+
+
+    #region encapsulamento
+    public float Life
     {
-        
+        get { return life; }
+        set { life = value; }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
+    }
+
+    private void Update()
+    {
+        float timer = 0.8f;
+        if(life < life/2)
+        {
+            anim.SetBool("broking", true);
+        }
+        if(life >= 0)
+        {
+            anim.SetTrigger("broke");
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
